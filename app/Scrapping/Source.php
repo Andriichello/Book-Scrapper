4b7df5e925791a5dfff697938dfd83f12e8318c5
Andrii Prykhodko <andriichello@gmail.com>
2021-06-23 12:08:58 +0300

    Add Source's static methods anotations.

--- app/Scrapping/Source.php
+++ app/Scrapping/Source.php
@@ -4,6 +4,13 @@ namespace App\Scrapping;
 
 use BenSampo\Enum\Enum;
 
+/**
+ * Class Source
+ * @package App\Scrapping
+ *
+ * @method static Source Bookclub()
+ * @method static Source Starylev()
+ */
 final class Source extends Enum
 {
     const Bookclub = 'bookclub';
