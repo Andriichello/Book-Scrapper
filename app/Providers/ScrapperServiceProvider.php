2e2c48cfb27eac3c7e109faa3923e312aa12898a
Andrii Prykhodko <andriichello@gmail.com>
2021-06-23 13:14:36 +0300

    Refactor bindings in ScrapperServiceProvider.

--- app/Providers/ScrapperServiceProvider.php
+++ app/Providers/ScrapperServiceProvider.php
@@ -16,16 +16,28 @@ class ScrapperServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->bind(Scrappers\Bookclub\BookScrapper::class, function () {
+            new Scrappers\Bookclub\BookScrapper($this->app->make(Parsers\Bookclub\BookParser::class));
+        });
+
+        $this->app->bind(Scrappers\Bookclub\AuthorScrapper::class, function () {
+            new Scrappers\Bookclub\AuthorScrapper($this->app->make(Parsers\Bookclub\AuthorParser::class));
+        });
+
+        $this->app->bind(Scrappers\Bookclub\GenreScrapper::class, function () {
+            new Scrappers\Bookclub\GenreScrapper($this->app->make(Parsers\Bookclub\GenreParser::class));
+        });
+
         $this->app->bind(Source::Bookclub . '-book-scrapper', function () {
-            return new Scrappers\Bookclub\BookScrapper($this->app->make(Parsers\Bookclub\BookParser::class));
+            return $this->app->make(Scrappers\Bookclub\BookScrapper::class);
         });
 
         $this->app->bind(Source::Bookclub . '-genre-scrapper', function () {
-            return new Scrappers\Bookclub\GenreScrapper($this->app->make(Parsers\Bookclub\GenreParser::class));
+            return $this->app->make(Scrappers\Bookclub\GenreScrapper::class);
         });
 
         $this->app->bind(Source::Bookclub . '-author-scrapper', function () {
-            return new Scrappers\Bookclub\AuthorScrapper($this->app->make(Parsers\Bookclub\AuthorParser::class));
+            return $this->app->make(Scrappers\Bookclub\GenreScrapper::class);
         });
     }
 
