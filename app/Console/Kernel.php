00303e82ee6bed4369334a901a518217381442d7
Andrii Prykhodko <andriichello@gmail.com>
2021-07-08 08:46:10 +0300

    Schedule daily authors, genres and books updates by bookclub-slug.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,15 @@
 
 namespace App\Console;
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Genre;
+use App\Models\Slug;
+use App\Services\Scrapping\Source;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
 
 class Kernel extends ConsoleKernel
 {
@@ -19,12 +26,55 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $slugableType = array_search(Author::class, Relation::$morphMap, true);
+
+            $authorSlugs = Slug::query()
+                ->where('source', Source::Bookclub)
+                ->where('slugable_type', $slugableType)
+                ->get();
+
+            foreach ($authorSlugs as $authorSlug) {
+                Artisan::call('scrape:bookclub-author', ['slug' => $authorSlug->slug]);
+            }
+        })->daily()
+            ->name('Updating authors by bookclub-slug')
+            ->withoutOverlapping();
+
+        $schedule->call(function () {
+            $slugableType = array_search(Genre::class, Relation::$morphMap, true);
+
+            $genreSlugs = Slug::query()
+                ->where('source', Source::Bookclub)
+                ->where('slugable_type', $slugableType)
+                ->get();
+
+            foreach ($genreSlugs as $genreSlug) {
+                Artisan::call('scrape:bookclub-genre', ['slug' => $genreSlug->slug]);
+            }
+        })->daily()
+            ->name('Updating genres by bookclub-slug')
+            ->withoutOverlapping();
+
+        $schedule->call(function () {
+            $slugableType = array_search(Book::class, Relation::$morphMap, true);
+
+            $bookSlugs = Slug::query()
+                ->where('source', Source::Bookclub)
+                ->where('slugable_type', $slugableType)
+                ->get();
+
+            foreach ($bookSlugs as $bookSlug) {
+                Artisan::call('scrape:bookclub-book', ['slug' => $bookSlug->slug]);
+            }
+        })->daily()
+            ->name('Updating books by bookclub-slug')
+            ->withoutOverlapping();
     }
 
     /**
@@ -34,7 +84,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
